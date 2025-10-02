3a7898418ee3c704332c1d4245b629b11f8785a4
Siddharth Karkhanis <skarkhanis95@gmail.com>
2025-10-02 08:21:08 +0000

    Terraform Cloud test run 2

--- infrastructure/test.tf
+++ infrastructure/test.tf
@@ -1,5 +1,15 @@
 resource "null_resource" "hello" {
+  # this trigger forces recreation if you change the message
+  triggers = {
+    message = "Hello from Terraform Cloud at ${timestamp()}"
+  }
+
   provisioner "local-exec" {
-    command = "echo Hello from Terraform Cloud!"
+    command = "echo \"${self.triggers.message}\""
   }
 }
+
+output "hello_message_trigger" {
+  value = null_resource.hello.triggers.message
+}
+
